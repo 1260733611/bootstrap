0ee0ae51ba1cba35a4d0ccab532899657d8caade
Johann-S <johann.servoire@gmail.com>
2018-03-02 14:15:20 +0100

    add code corage

--- js/tests/karma.conf.js
+++ js/tests/karma.conf.js
@@ -11,18 +11,19 @@ module.exports = (config) => {
       'karma-chrome-launcher',
       'karma-firefox-launcher',
       'karma-qunit',
-      'karma-detect-browsers'
+      'karma-detect-browsers',
+      'karma-coverage-istanbul-reporter'
     ],
     // list of files / patterns to load in the browser
     files: [
       jqueryFile,
       'assets/js/vendor/popper.min.js',
-      'js/dist/util.js',
-      'js/dist/tooltip.js',
-      'js/dist/!(util|index|tooltip).js', // include all of our js/dist files except util.js, index.js and tooltip.js
+      'js/dist/coverage/util.js',
+      'js/dist/coverage/tooltip.js',
+      'js/dist/coverage/!(util|index|tooltip).js', // include all of our js/dist files except util.js, index.js and tooltip.js
       'js/tests/unit/*.js'
     ],
-    reporters: ['dots'],
+    reporters: ['dots', 'coverage-istanbul'],
     port: 9876,
     colors: true,
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
@@ -48,6 +49,19 @@ module.exports = (config) => {
         }
 
         throw new Error('Please install Firefox or Chrome')
+      }
+    },
+    coverageIstanbulReporter: {
+      reports: ['lcov', 'text-summary'],
+      fixWebpackSourcePaths: true,
+      thresholds: {
+        emitWarning: true,
+        global: {
+          statements: 80,
+          lines: 80,
+          branches: 80,
+          functions: 80
+        }
       }
     }
   })
